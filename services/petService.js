ab5c06330372cef51250307a24c7770d18bf4618
mac <aidahimmiche@gmail.com>
2021-01-03 01:44:43 +0100

    Models and Services

--- services/petService.js
+++ services/petService.js
@@ -1,36 +1,48 @@
 const db = require ('../models')
 const Sequelize = require('sequelize')
 
-async function createPet (username, petname, petCateg, isV, sex){
-    user = await db.User.findOne({where: {username: username, role: 'petowner'}})
-    await db.Pet.create({
-        petName: petname,
-        petCategory: petCateg,
-        isVaccinated: isV,
-        sex: sex,
-        petOwner_id: user.id
-    })
-}
-async function getAllPetsOfOwner (username){
-    user = await db.User.findOne({where: {username: username, role: 'petowner'}})
-    return await db.Pet.findAll({
-        where: {
+async function createPet (res, username, petname, petCateg, isV, sex){
+    let user = await db.User.findOne({where: {username: username, role: 'petowner'}})
+    if(user !== null ) {
+        await db.Pet.create({
+            petName: petname,
+            petCategory: petCateg,
+            isVaccinated: isV,
+            sex: sex,
             petOwner_id: user.id
-        }
-    })
+        })
+    }else {
+        res.send('Username Not Found')
+    }
+}
+async function getAllPetsOfOwner (res, username){
+    let user = await db.User.findOne({where: {username: username, role: 'petowner'}})
+    if(user !== null) {
+        return await db.Pet.findAll({
+            where: {
+                petOwner_id: user.id
+            }
+        })
+    }else{
+        res.send('Username Not Found')
+    }
 }
 
-async function updatePetVStatus (username, isV){
-    user = await db.User.findOne({where: {username: username, role: 'petowner'}})
-    return await db.Pet.update({isVaccinated: isV},{
-        where: {
-            petOwner_id: user.id
-        }
-    })
+async function updatePetVStatus (res, username, isV){
+    let user = await db.User.findOne({where: {username: username, role: 'petowner'}})
+    if(user!== null) {
+        return await db.Pet.update({isVaccinated: isV}, {
+            where: {
+                petOwner_id: user.id
+            }
+        })
+    }else {
+        res.send('Username Not Found')
+    }
 }
 
 async function deletePet (username, pName){
-    user = await db.User.findOne({where: {username: username, role: 'petowner'}})
+    let user = await db.User.findOne({where: {username: username, role: 'petowner'}})
     return await db.Pet.destroy({
         where: {
             petOwner_id: user.id,
